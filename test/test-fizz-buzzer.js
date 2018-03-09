db3b35109b10fc7fde0f02199436057b77c58791
Julie K <julieskim160@gmail.com>
2018-03-09 12:38:32 -0800

    fizzbuzz

--- test/test-fizz-buzzer.js
+++ test/test-fizz-buzzer.js
@@ -1,17 +1,31 @@
-module.exports = function(num) {
-	if (typeof num !== 'number') {
-		throw Error('`num` must be a number');
-	}
-	if (num % 15 === 0) {
-		return 'fizz-buzz';
-	}
-	if (num % 5 === 0) {
-		return 'buzz';
-	}
-	if (num % 3 === 0) {
-		return 'fizz';
-	}
-	else {
-		return num;
-	}
-}
+'use strict';
+const expect = require('chai').expect;
+
+// import adder
+const fizzBuzzer = require('../fizzBuzzer');
+
+// unit tests for our `adder` function
+describe('fizzBuzzer', function() {
+
+
+  it('should perform fizzBuzz', function() {
+    const normalCases = [
+      {a:15, expected:'fizz-buzz'},
+      {a:5, expected:'buzz'},
+      {a:3, expected:'fizz'}
+    ];
+    normalCases.forEach(function(input) {
+      const answer = fizzBuzzer(input.a);
+      expect(answer).that.equal(input.expected);
+    });
+  });
+
+  it('should raise error if args not numbers', function() {
+    const badInputs = ['a', '1'];
+    badInputs.forEach(function(input) {
+      expect(function() {
+        fizzBuzzer(input);
+      }).to.throw(Error);
+    });
+  });
+});
